d368a0c6bc31547ae488921d8280e13fa15e439a
Vagul Mahadevan <vagulgm@gmail.com>
2022-12-23 14:22:05 -0500

    added initial code from email

--- cloudmesh/report/report.py
+++ cloudmesh/report/report.py
@@ -1,1 +1,30 @@
 # tbd
+
+
+a = """
+\documentclass{article}
+
+\usepackage{graphicx}
+
+
+\begin{document}
+"""
+
+
+images = determin list of image of image files
+for i in images:
+  filename =  put value here and derive from images
+
+  image = f"""
+  \begin{{figure}}
+  \centering
+  \includegraphics{({filename)}}
+  \caption{{{Histogram of {filename}}}}
+  label{{fig:{filename}}}
+  \end{figure}
+  """
+
+c =
+"""
+\ens{document}
+"""
