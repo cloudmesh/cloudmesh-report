19ff7c89d328b46a3cb3470b35999b8bb4944a59
Gregor von Laszewski <laszewski@gmail.com>
2022-12-23 14:38:59 -0500

    update first draft

--- cloudmesh/report/command/report.py
+++ cloudmesh/report/command/report.py
@@ -35,42 +35,10 @@ class ReportCommand(PluginCommand):
         """
 
 
-        map_parameters(arguments, "file", "parameter", "experiment")
+        # map_parameters(arguments, "FILE")
 
         VERBOSE(arguments)
 
-        banner("rewriting arguments so we convert to appropriate types for easier handeling", color="RED")
-
-        arguments = Parameter.parse(arguments,
-                                    parameter='expand',
-                                    experiment='dict',
-                                    COMMAND='str')
-
-
-        VERBOSE(arguments)
-
-        banner("showcasing tom simple if parsing based on teh dotdict", color="RED")
-
-
-        #
-        # It is important to keep the programming here to a minimum and any substantial programming ought
-        # to be conducted in a separate class outside the command parameter manipulation. If between the
-        # elif statement you have more than 10 lines, you may consider putting it in a class that you import
-        # here and have propper methods in that class to handle the functionality. See the Manager class for
-        # an example.
-        #
-
-        if arguments.file:
-            print("option a")
-
-        elif arguments.list:
-            print("option b")
-
-
-        Console.error("This is just a sample of an error")
-        Console.warning("This is just a sample of a warning")
-        Console.info("This is just a sample of an info")
-
-        Console.info(" You can witch debugging on and off with 'cms debug on' or 'cms debug off'")
+        r = Report(filename=filename)
 
         return ""
--- cloudmesh/report/report.py
+++ cloudmesh/report/report.py
@@ -1,30 +1,80 @@
-# tbd
+import textwrap
 
+from cloudmesh.common.FlatDict import read_config_parameters
 
-a = """
-\documentclass{article}
-
-\usepackage{graphicx}
-
-
-\begin{document}
+testcase = """
+title: My report
+introduction:
+   - this-is-a-good-filename1.pdf
+     - caption: my fig 1
+     - label: "fig:1"
+   - this-is-a-good-filename2.pdf
+     - caption: my fig 2
+     - label: "fig:2"
+   - this-is-a-good-filename3.pdf
+   - this-is-a-good-filename4.pdf
 """
 
+class Report:
 
-images = determin list of image of image files
-for i in images:
-  filename =  put value here and derive from images
+  begin_document = \
+    textwrap.dedent("""
+    \documentclass{article}
 
-  image = f"""
-  \begin{{figure}}
-  \centering
-  \includegraphics{({filename)}}
-  \caption{{{Histogram of {filename}}}}
-  label{{fig:{filename}}}
-  \end{figure}
-  """
+    \usepackage{graphicx}
 
-c =
-"""
-\ens{document}
-"""
+    \begin{document}
+    """)
+
+  end_document = \
+    textwrap.dedent("""
+    \end{document}
+    """
+
+  def generate_image(self,
+                     filename,
+                     caption=None,
+                     label=None,
+                     ):
+    if caption is None:
+      caption = filename
+    if label is None:
+      label = f"fig:{self.counter}"
+    image = \
+      textwrap.dedent("""
+      \begin{{figure}}
+      \centering
+      \includegraphics{({filename)}}
+      \caption{{{caption}}}
+      label{{fig:{filename}}}
+      \end{figure}
+      """)
+    self.counter = self.counter + 1
+    return image
+
+  def __init__(self, directory="./", config):
+    self.counter = 0
+    self.config = config
+    self.directory = directory
+
+  def generate(self):
+    #mages = determin list of image of image files
+    for i in self.images:
+      self.generate_image(images.)
+      filename =  put value here and derive from images
+
+
+  def read_config(self):
+      filename = self.config
+      # TBD Vagul
+      config_dict = read_config_parameters(filename)
+      title = config_dict["title"]
+      images = []
+      for key in config_dict["introduction"]:
+        images.append(config_dict)
+
+
+
+
+
+
